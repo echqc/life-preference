4d37de1b14c38f4e15b24476ab31d42429dab3ad
itcast <hello@itcast.cn>
2026-09-21 23:20:11 +0800

    实现用户签到功能

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -14,14 +15,19 @@ import com.hmdp.service.IUserService;
 import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RegexUtils;
 import com.hmdp.utils.SystemConstants;
+import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -80,6 +86,56 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         return Result.ok(token);
 
     }
+
+    @Override
+    public Result sign() {
+        //获取当前登录用户
+        Long userId = UserHolder.getUser().getId();
+        //获取当前日期
+        LocalDateTime now = LocalDateTime.now();
+        // 拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyy:MM"));
+        String key = RedisConstants.USER_SIGN_KEY + userId + keySuffix;
+        // 获取今天是本月的第几天（offset = dayOfMonth - 1）
+        int dayOfMonth = now.getDayOfMonth();
+        // 写入redis完成签到 SETBIT key offset 1
+        stringRedisTemplate.opsForValue().setBit(key, dayOfMonth - 1, true);
+        return Result.ok();
+    }
+
+    @Override
+    public Result signCount() {
+        // 获取当前登录用户
+        Long userId = UserHolder.getUser().getId();
+        // 获取当前日期
+        LocalDateTime now = LocalDateTime.now();
+        // 拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyy:MM"));
+        String key = RedisConstants.USER_SIGN_KEY + userId + keySuffix;
+        // 获取今天是本月的第几天（offset）
+        int dayOfMonth = now.getDayOfMonth();
+        // 获取本月截止今天的所有签到记录，返回一个十进制的数字 BITFIELD key GET u[dayOfMonth] 0
+        List<Long> results = stringRedisTemplate.opsForValue().bitField(
+                key,
+                BitFieldSubCommands.create().get(BitFieldSubCommands.BitFieldType.unsigned(dayOfMonth)).valueAt(0)
+        );
+        // 没有任何签到结果
+        if (CollUtil.isEmpty(results)) {
+            return Result.ok(0);
+        }
+        long num = results.get(0).longValue();
+        if (num == 0) return Result.ok(0);
+        // 循环遍历，计数统计
+        int count = 0;
+        while ((num & 1) != 0) {
+            // 如果最后一位不为0，说明已签到，计数器+1
+            count++;
+            // 把数字右移一位，抛弃最后一位，继续下一个位
+            num >>>= 1; // 因为java中的整形都是有符号数，>>>是无符号右移，左边最高位补0，如果是>>最高位补符号位，对于正数来说，无符号右移和有符号右移结果相同
+        }
+        return Result.ok(count);
+    }
+
     private User createUserWithPhone(String phone){
         User user = new User();
         user.setPhone(phone);
